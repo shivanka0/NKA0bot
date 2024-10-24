2834cacacf73519b22566b1968e9d3caf79edc40
ShivaNKA0 <shiva.kanneboina@gmail.com>
2024-10-24 11:09:42 -0400

    ui and backend bugs

--- api/index.js
+++ api/index.js
@@ -1,7 +1,3 @@
-require("dotenv").config({ path: __dirname + "/.env" });
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
 const { twitterClient } = require("./twitterClient.js")
 const CronJob = require("cron").CronJob;
 
@@ -12,23 +8,7 @@ const tweet = async (tweet) => {
 		console.log(e)
 	}
 }
-export default async function handler(req, res) {
-	if (req.method === 'POST') {
-	  const { tweetText } = req.body;
-	  
-	  try {
-		// Post the tweet
-		await twitterClient.v2.tweet(tweetText);
-		res.status(200).json({ success: true, message: 'Tweet posted!' });
-	  } catch (error) {
-		console.error('Error posting tweet:', error);
-		res.status(500).json({ success: false, error: error.message });
-	  }
-	} else {
-	  res.status(405).json({ message: 'Only POST requests allowed' });
-	}
-  }
-tweet("tweet this in 30 seconds, again shivanka0");
+tweet("hello")
 
 // tweet();
 
